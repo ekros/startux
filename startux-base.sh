9aa1af5f99a4f129fcac18e955613f85e09e51de
Eric Ros <ekros@outlook.com>
2016-06-09 22:49:06 +0200

    Add red color to echoes

--- startux-base.sh
+++ startux-base.sh
@@ -3,56 +3,56 @@
 # DO NOT RUN THIS SCRIPT AS ROOT
 
 # profile backup
-echo "Making a backup of your .profile file..."
+echo -e "\e[1;31mMaking a backup of your .profile file...\e[0m"
 cp -f $HOME/.profile $HOME/.profile.backup
 
 # Update repositories
-echo Retrieving list of packages...
+echo -e "\e[1;31mRetrieving list of packages...\e[0m"
 sudo apt-get -qq update
 
 # Install Terminator. A super-killer terminal
-echo Installing Terminator...
+echo -e "\e[1;31mInstalling Terminator...\e[0m"
 sudo apt-get -y install terminator
 
 # Install curl
-echo Installing curl...
+echo -e "\e[1;31mInstalling curl...\e[0m"
 sudo apt-get -y install curl
 
 # Install VIM
-echo Installing Vim...
+echo -e "\e[1;31mInstalling Vim...\e[0m"
 sudo apt-get -y install vim
 
 # Install Silver searcher (replacement for find)
-echo Installing ag...
+echo -e "\e[1;31mInstalling ag...\e[0m"
 sudo apt-get -y install silversearcher-ag
 
 # Install htop (convenient replacement for ps)
-echo Installing htop...
+echo -e "\e[1;31mInstalling htop...\e[0m"
 sudo apt-get -y install htop
 
 # Install at (application to  execute commands at a given time)
-echo Installing at...
+echo -e "\e[1;31mInstalling at...\e[0m"
 sudo apt-get -y install at
 
 # Install Remmina (RDP / VNC client)
-echo Installing Remmina...
+echo -e "\e[1;31mInstalling Remmina...\e[0m"
 sudo apt-get -y install remmina
 
 # Install Atom
-echo Installing Atom...
+echo -e "\e[1;31mInstalling Atom...\e[0m"
 # TODO ONLY DO THIS IF NOT INSTALLED
 wget https://github.com/atom/atom/releases/download/v1.7.4/atom-amd64.deb  && sudo dpkg -i atom-amd64.deb
 
-# Install npm , necessary to install atom packages
-echo Installing npm...
+# Install npm, necessary to install atompackages
+echo -e "\e[1;31mInstalling npm...\e[0m"
 sudo apt-get -y install npm
 
-# Install Atom packages
-echo Installing Atom extra packages...
+# Install Atompackages
+echo -e "\e[1;31mInstalling Atomextra packages...\e[0m"
 apm install atom-beautify color-picker emmet highlight-selected javascript-snippets meteor-helper meteor-snippets react
 
 # aliases
-echo Setting git aliases... lol st co br ci...
+echo "Setting git aliases... lol st co br ci...\e[0m"
 alias gg="git status && git log | head"
 echo "alias gg=\"git status && git log | head\"" >> $HOME/.bashrc
 
@@ -64,10 +64,10 @@ git config --global alias.br branch
 git config --global alias.ci commit
 
 #install bash-git-prompt, an informative bash prompt
-echo Installing bash-git-prompt...
+echo -e "\e[1;31mInstalling bash-git-prompt...\e[0m"
 cd ~
 git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
-echo "source ~/.bash-git-prompt/gitprompt.sh" >> $HOME/.bashrc
+echo -e "source ~/.bash-git-prompt/gitprompt.sh" >> $HOME/.bashrc
 GIT_PROMPT_ONLY_IN_REPO=1
 
 # Post execution steps:
