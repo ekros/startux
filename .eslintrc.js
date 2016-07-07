92bbac526dd6b66e7bd250e18ecee2dc8123ba4c
Eric Ros <ekros@outlook.com>
2016-07-07 13:10:17 +0200

    Add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "warn",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "comma-dangle": [
+            "warn",
+            "always-multiline"
+        ]
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # startux
 Collection of scripts for development setup in Ubuntu
+
+In addition to this, there are some useful files, like a ESLint config file.
