886d782f1c4cc75bcd28ce7e6d41cef8d7ee1ba2
Eric Ros <ekros@outlook.com>
2017-12-13 18:26:33 +0100

    Add git l alias

--- startux-base.sh
+++ startux-base.sh
@@ -113,6 +113,7 @@ git config --global alias.st status
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
+git config --global alias.l "log --stat"
 # git customizations
 git config --global help.autocorrect 50
 git config --global color.status.changed "white yellow bold"
