1aff534ec18a7d75d89e496f4521f4636d977754
Eric Ros <ekros@outlook.com>
2018-06-15 13:32:57 +0200

    Use apt instead of apt-get. Install atom with apt.

--- startux-base.sh
+++ startux-base.sh
@@ -8,43 +8,43 @@ cp -f $HOME/.profile $HOME/.profile.backup
 
 # Update repositories
 echo -e "\e[1;31mRetrieving list of packages...\e[0m"
-sudo apt-get -qq update
+sudo apt -qq update
 
 # Install Terminator. A super-killer terminal
 echo -e "\e[1;31mInstalling Terminator...\e[0m"
-sudo apt-get -y install terminator
+sudo apt -y install terminator
 
 # Install curl
 echo -e "\e[1;31mInstalling curl...\e[0m"
-sudo apt-get -y install curl
+sudo apt -y install curl
 
 # Install VIM
 echo -e "\e[1;31mInstalling Vim...\e[0m"
-sudo apt-get -y install vim
+sudo apt -y install vim
 
 # Install Silver searcher (replacement for find)
 echo -e "\e[1;31mInstalling ag...\e[0m"
-sudo apt-get -y install silversearcher-ag
+sudo apt -y install silversearcher-ag
 
 # Install htop (convenient replacement for ps)
 echo -e "\e[1;31mInstalling htop...\e[0m"
-sudo apt-get -y install htop
+sudo apt -y install htop
 
 # Install at (application to  execute commands at a given time)
 echo -e "\e[1;31mInstalling at...\e[0m"
-sudo apt-get -y install at
+sudo apt -y install at
 
 # Install Remmina (RDP / VNC client)
 echo -e "\e[1;31mInstalling Remmina...\e[0m"
-sudo apt-get -y install remmina
+sudo apt -y install remmina
 
 # Install Trash, like rm but sends to trash
 echo -e "\e[1;31mInstalling Trash...\e[0m"
-sudo apt-get -y install trash
+sudo apt -y install trash
 
 # Install zsh, an alternative customizable bash shell
 echo -e "\e[1;31mInstalling Zsh...\e[0m"
-sudo apt-get -y install Zsh
+sudo apt -y install Zsh
 
 # Set zsh as default shell
 echo -e "\e[1;31mSetting Zsh as default shell...\e[0m"
@@ -57,19 +57,20 @@ curl -L http://install.ohmyz.sh | sh
 # Install Atom
 echo -e "\e[1;31mInstalling Atom...\e[0m"
 # TODO ONLY DO THIS IF NOT INSTALLED
-wget https://github.com/atom/atom/releases/download/v1.7.4/atom-amd64.deb  && sudo dpkg -i atom-amd64.deb
+#wget https://github.com/atom/atom/releases/download/v1.7.4/atom-amd64.deb  && sudo dpkg -i atom-amd64.deb
+sudo apt install atom
 
 # Install npm, necessary to install atompackages
 echo -e "\e[1;31mInstalling npm...\e[0m"
-sudo apt-get -y install npm
+sudo apt -y install npm
 
 # Install Meld, a graphical diff tool
 echo -e "\e[1;31mInstalling Meld...\e[0m"
-sudo apt-get -y install meld
+sudo apt -y install meld
 
 # Install Zeal, an offline developer docs
 echo -e "\e[1;31mInstalling Zeal...\e[0m"
-sudo apt-get install zeal
+sudo apt install zeal
 
 # Install tig, git commits browser
 echo -e "\e[1;31mInstalling tig...\e[0m"
